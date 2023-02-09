5893757de04355da9845ddc271ef3c4c864557ed
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2023-02-09 20:39:34 +0300

    working on nvabar

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import './header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBed, faPlane, faCar, faTaxi} from '@fortawesome/free-solid-svg-icons'
+
+
+
+
+function Header() {
+
+
+
+  return (
+
+    <>
+
+        <div className="header">
+            <div className="headerlist">
+
+                <div className="headerlistitem">
+
+                    
+                    <FontAwesomeIcon icon={faBed}/>
+                    <span>Stays</span>
+                </div>
+
+                <div className="headerlistitem">
+
+                    
+                    <FontAwesomeIcon icon={faPlane}/>
+                    <span>Flights</span>
+                </div>
+
+                <div className="headerlistitem">
+
+                    
+                    <FontAwesomeIcon icon={faCar}/>
+                    <span>Car rentals</span>
+                </div>
+
+                <div className="headerlistitem">
+
+                    
+                    <FontAwesomeIcon icon={faBed}/>
+                    <span>Attractions</span>
+                </div>
+
+                <div className="headerlistitem">
+
+                    
+                    <FontAwesomeIcon icon={faTaxi}/>
+                    <span>Airport Taxi</span>
+                </div>
+            </div>
+        </div>
+            
+
+    </>
+
+
+  )
+}
+
+export default Header
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+  return (
+
+    <>
+    
+        <div className="navbar">
+            <div className="navcontainer">
+                <span className="logo">Rawa</span>    
+
+                <div className="navitems">
+
+                    <button className="navbutton">Register</button>
+                    <button className="navbutton">LogIn</button>
+
+
+                </div>
+                 
+            </div>      
+
+        </div>
+
+
+
+            
+
+
+    </>
+
+
+  )
+}
+
+export default Navbar
