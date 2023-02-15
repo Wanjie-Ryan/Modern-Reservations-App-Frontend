9d1b3bbc6ec8af8796ef96e8b692b728290c633b
Wanjie Ryan <104978200+Wanjie-Ryan@users.noreply.github.com>
2023-02-15 08:59:37 +0300

    working on the home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,37 +1,24 @@
-import React from 'react'
-import './home.css'
-import Navbar from '../../components/navbar/Navbar'
-import Header from '../../components/header/Header'
-import Featured from '../../components/featured/featured'
+import React from "react";
+import "./home.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import Featured from "../../components/featured/featured";
 
 const Home = () => {
-
-
-    
   return (
     <>
+      <div>
+        <Navbar />
+        <Header />
 
-
-        <div>
-          <Navbar/>
-          <Header/>
-
-          <div className ='homecontainer'>
-
-            <Featured/>
-
-
-          </div>
-
-
-
-
+        <div className="homecontainer">
+          <Featured />
+          
 
         </div>
-
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
