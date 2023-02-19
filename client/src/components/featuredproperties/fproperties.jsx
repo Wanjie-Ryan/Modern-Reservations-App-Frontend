2d608a83a3022fa82879e7a67b3f60a47a9df736
Ryan <ryanwanjiedistortion@gmail.com>
2023-02-19 20:57:03 +0300

    featured properties almost

--- client/src/components/featuredproperties/fproperties.jsx
+++ client/src/components/featuredproperties/fproperties.jsx
@@ -11,17 +11,75 @@ function Fproperties() {
   return (
 
     <>
+
+
+            <div className="fp">
     
-        <div className="fp">
+                <div className="fpitem">
 
-            <img src="" alt="" className="fpImg" />
+                    <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="" className="fpImg" />
+
+                    <span className="fpname">Aparthotel Stare Miasto</span>
+                    <span className="fpcity">Madrid</span>
+                    <span className="fpprice">starting from $120</span>
+                    <div className="fprating">
+                        <button>8.9</button>
+                        <span>Excellent</span>
+                    </div>
+
+                </div>
 
 
 
+                <div className="fpitem">
 
-        </div>
-    
-    
+                    <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="" className="fpImg" />
+
+                    <span className="fpname">Aparthotel Stare Miasto</span>
+                    <span className="fpcity">Madrid</span>
+                    <span className="fpprice">starting from $120</span>
+                    <div className="fprating">
+                        <button>8.9</button>
+                        <span>Excellent</span>
+                    </div>
+
+                </div>
+
+
+
+                <div className="fpitem">
+
+                    <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="" className="fpImg" />
+
+                    <span className="fpname">Aparthotel Stare Miasto</span>
+                    <span className="fpcity">Madrid</span>
+                    <span className="fpprice">starting from $120</span>
+                    <div className="fprating">
+                        <button>8.9</button>
+                        <span>Excellent</span>
+                    </div>
+
+                </div>
+
+
+
+                <div className="fpitem">
+
+                    <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="" className="fpImg" />
+
+                    <span className="fpname">Aparthotel Stare Miasto</span>
+                    <span className="fpcity">Madrid</span>
+                    <span className="fpprice">starting from $120</span>
+                    <div className="fprating">
+                        <button>8.9</button>
+                        <span>Excellent</span>
+                    </div>
+
+                </div>
+
+            </div>
+            
+
     </>
 
 
