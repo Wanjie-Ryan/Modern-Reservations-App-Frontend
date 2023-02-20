3c49de3a3042fdfd7f93c35ac0b88c05ac410982
Ryan <ryanwanjiedistortion@gmail.com>
2023-02-20 15:36:20 +0300

    search page underway

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
-
+import './list.css'
 
 
 
@@ -13,9 +13,47 @@ function List() {
 
     <>
 
-    {/* the type is a prop that checks if list is there then the header is removed */}
+        {/* the type is a prop that checks if list is there then the header is removed */}
     
-    <div><Navbar/> <Header type ='list'/></div>
+        <div><Navbar/> <Header type ='list'/></div>
+
+        <div className="listcontainer">
+
+          <div className="listwrapper">
+
+            <div className="listsearch">
+
+              <h1 className="lstitle">Search</h1>
+
+              <div className="lsitem">
+                <label>Destination</label>
+
+              </div>
+
+
+              <div className="lsitem">
+                <label>Destination</label>
+                
+              </div>
+
+
+            </div>
+
+
+            <div className="listresult">
+
+
+
+            </div>
+
+
+          </div>
+
+
+
+
+
+        </div>
     
     
     </>
