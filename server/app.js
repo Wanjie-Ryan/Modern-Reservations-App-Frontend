785c2874a1758aecea557cf2f121cea51acf9c36
Ryan <ryanwanjiedistortion@gmail.com>
2023-02-25 08:08:35 +0300

    connection to DB

--- server/app.js
+++ server/app.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
-const port = 3001
+const port = process.env.PORT || 3001
+const connectionDB = require('./DB/connection')
 
 
 
-app.listen({port}, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
+
+
+
+
+
+const DB = async()=>{
+
+    try{
+
+        await connectionDB(process.env.mongo_url)
+
+        app.listen({port}, ()=>{
+            console.log(`Server is running on port ${port}`)
+        })
+    }
+
+    catch(error){
+
+
+        console.log(error)
+    }
+}
+
+DB()
+
+
+
 
