2a3942e1b3eb664cd8c62d0c41d4039fb19c0930
Ryan <ryanwanjiedistortion@gmail.com>
2023-02-28 08:53:04 +0300

    count by type frontend

--- client/src/components/propertylist/propertylist.jsx
+++ client/src/components/propertylist/propertylist.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import './propertylist.css'
+import usefetch from '../../hooks/usefetch'
+
+
 
 function propertylist() {
+
+   const {data, loading, error} = usefetch('http://localhost:3001/api/hotels/countbytype')
+
+   console.log(data)
+
+
+
   return (
 
 
@@ -9,9 +19,9 @@ function propertylist() {
     <>
     
     
-    <div className="plist">
+     <div className="plist">
 
-        <div className="plistitem">
+     {loading ? 'loading,please wait...' : <><div className="plistitem">
 
 
            <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" alt="" className="plistimg" /> 
@@ -19,7 +29,7 @@ function propertylist() {
            <div className="plisttitles">
 
                 <h1>Hotels</h1>
-                <h1>2331 Hotels</h1>
+                <h1>{data.count}</h1>
 
 
            </div>
@@ -86,14 +96,9 @@ function propertylist() {
 
 
            </div>
-        </div>
 
 
-
-
-
-
-
+        </div></>}
 
 
     </div>
