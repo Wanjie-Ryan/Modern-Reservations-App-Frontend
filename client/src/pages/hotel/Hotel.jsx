0eec9d12c58a0c7a30b67897d4736c094887cfa0
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-04 08:49:40 +0300

    single page

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -6,36 +6,51 @@ import Mail from '../../components/maillist/maillist'
 import Footer from '../../components/footer/footer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faLocationDot, faCircleXmark, faCircleArrowLeft, faCircleArrowRight} from '@fortawesome/free-solid-svg-icons'
-
+import usefetch from '../../hooks/usefetch'
+import {useLocation} from  'react-router-dom'
 
 
 function Hotel() {
 
+  const location = useLocation()
+
+  //the uselocation hook is used to give details about the page you are in.
+
+
+  // console.log(location)
+
+  const id = location.pathname.split('/')[2]
+
+  // console.log(id)
 
   const [slideindex, setslideindex] = useState(0)
   const [open, setopen] = useState(false)
+  const {data, loading, error} = usefetch(`http://localhost:3001/api/hotels/find/${id}`)
 
 
-  const photos = [
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
-    },
-  ];
+
+
+
+  // const photos = [
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1",
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1",
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1",
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1",
+  //   },
+  //   {
+  //     src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
+  //   },
+  // ];
 
 
   const Handleopen =(i) =>{
@@ -74,7 +89,7 @@ function Hotel() {
       </div>
 
 
-      <div className='hotelcontainer'>
+      {loading ? <h2>Loading, Please Wait...</h2> : <> <div className='hotelcontainer'>
 
         {open && <div className='slider'>
 
@@ -83,7 +98,7 @@ function Hotel() {
           <FontAwesomeIcon icon = {faCircleArrowLeft} className ='arrow' onClick={()=>{handlemove('l')}}/>
 
           <div className="sliderwrapper">
-            <img src={photos[slideindex].src} alt="" className="sliderimg" />
+            <img src={data.photos[slideindex]} alt="" className="sliderimg" />
           </div>
           <FontAwesomeIcon icon = {faCircleArrowRight} className ='arrow' onClick={()=>{handlemove('r')}}/>
 
@@ -92,27 +107,27 @@ function Hotel() {
 
         <div className="hotelwrapper">
           <button className="booknow">Reserve or Book Now!</button>
-          <h1 className="hoteltitle">Grand Hotel</h1>
+          <h1 className="hoteltitle">{data.name}</h1>
           <div className="hoteladdress">
 
             <FontAwesomeIcon icon = {faLocationDot}/>
-            <span>Elton St 125 New York.</span>
+            <span>{data.address}.</span>
 
           </div>
 
             <span className="hoteldistance">
-              Excellent location - 500m from center
+             {data.distance}
             </span>
 
             <span className="hotelpricehighlight">
-              Book a stay over $114 at this property and get a free airport taxi.
+              Book a stay over ${data.cheapestprice} at this property and get a free airport taxi.
             </span>
 
             <div className="hotelimages">
 
-              {photos.map((photo,i)=>(
+              {data.photos?.map((photo,i)=>(
                 <div className="hotelimagewrapper">
-                  <img onClick ={()=>Handleopen(i)}  src={photo.src} alt ='' className ='img'/>
+                  <img onClick ={()=>Handleopen(i)}  src={photo} alt ='' className ='img'/>
                 </div>
 
                   //whenever we click an image  we update our state and give its index
@@ -124,20 +139,10 @@ function Hotel() {
 
               <div className="hoteldetailstext">
 
-                <h1 className="hoteltitle">Stay in the heart of Krakow</h1>
+                <h1 className="hoteltitle">{data.title}</h1>
                 <p className="hoteldesc">
 
-                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
-                Street Apartments has accommodations with air conditioning and
-                free WiFi. The units come with hardwood floors and feature a
-                fully equipped kitchenette with a microwave, a flat-screen TV,
-                and a private bathroom with shower and a hairdryer. A fridge is
-                also offered, as well as an electric tea pot and a coffee
-                machine. Popular points of interest near the apartment include
-                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                airport is John Paul II International Kraków–Balice, 16.1 km
-                from Tower Street Apartments, and the property offers a paid
-                airport shuttle service.
+                {data.description}
                 </p>
 
               </div>
@@ -168,7 +173,7 @@ function Hotel() {
 
 
         </div>
-      </div>
+      </div></>}
 
 
 
