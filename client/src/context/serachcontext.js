a68df52fb36deeee40a3f3c4cc0e402d99cbd63c
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-14 14:39:44 +0300

    search context

--- client/src/context/serachcontext.js
+++ client/src/context/serachcontext.js
@@ -1,4 +1,4 @@
-import React, {createContext} from 'react';
+import React, {createContext, useReducer} from 'react';
 
 
 const initialstate ={
@@ -23,5 +23,26 @@ export const Searchcontext = React.createContext(initialstate)
 
     switch(action.type){
         case 'newsearch': 
+            return action.payload
+
+        //whenever our search bar changes we will send this data, the payload will contain the data
+
+        case 'resetsearch':
+            return initialstate
+
+            default:
+                return state;
     }
+ }
+
+
+ export const SearchcontextProvider = ({children}) =>{
+
+    const [state, dispatch] = useReducer(searchreducer, initialstate)
+
+    return(
+        <Searchcontext.Provider value = {{city:state.city, dates:state.dates, options:state.options, dispatch}}>
+            {children}
+        </Searchcontext.Provider>
+    )
  }
