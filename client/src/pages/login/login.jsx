37597162c57ad50e70851d0e27df5124129bd663
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-16 13:47:50 +0300

    login component

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,14 +1,69 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import { Authcontext } from '../../context/authcontext'
 import './login.css'
 
 
 function Login() {
 
 
+    const [credentials, setcredentials] = useState({
+
+        username:undefined,
+        password:undefined
+    })
+
+
+    const {loading, error, dispatch} = useContext(Authcontext)
+
+
+    const handlechange = ()=>{
+
+
+    }
+
+
+
+
   return (
 
 
-    <div>login</div>
+    <>
+
+
+        <div className="login">
+
+
+            <div className="lcont">
+
+                <input type="text" placeholder='username' onChange ={handlechange} id="username" className='input'/>
+
+                <input type="password" placeholder='password' onChange ={handlechange} id="password" className='input'/>
+
+                <button className="bnt">Login</button>
+
+                {error && <span>{error.message}</span>}
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+    </>
+    
 
 
   )
