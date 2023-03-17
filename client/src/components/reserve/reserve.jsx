37bf209a3d5fcf49879787768d87cbfee6a87082
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-17 14:49:51 +0300

    rooms

--- client/src/components/reserve/reserve.jsx
+++ client/src/components/reserve/reserve.jsx
@@ -7,12 +7,12 @@ import usefetch from '../../hooks/usefetch'
 
 
 
-function Reserve({setopen, id}) {
+function Reserve({setopen, hotelid}) {
 
     //set open is passed as a prop for the modal opening and closing while id is the id for the specific hotel that has been opened.
 
 
-    const {data, loading, error} = usefetch(`http://localhost:3001/api/hotels/room/${id}`)
+    const {data, loading, error} = usefetch(`http://localhost:3001/api/hotels/room/${hotelid}`)
 
     console.log(data)
 
@@ -31,19 +31,31 @@ function Reserve({setopen, id}) {
 
                 <span>Select your rooms: </span>
 
-                {data.map(item=>{
+                {data.map((item)=>(
 
                   <div className="ritem">
 
                     <div className="rtitle">{item.title}</div>
                     <div className="rdesc">{item.description}</div>
+                    <div className="rmax">Max People: <b>{item.maxpeople}</b></div>
 
+                    <div className="rprice">{item.price}</div>
+
+                    <div className="room">
+
+                      {item.roomnumbers.map(num=>{
+
+
+                      })}
+
+
+                    </div>
 
 
                   </div>
 
 
-                })}
+                ))}
 
 
 
