60d0912e04fb8c3b827666e5d962b03cddc556e8
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-17 14:18:05 +0300

    slight error in fetching data

--- client/src/components/reserve/reserve.jsx
+++ client/src/components/reserve/reserve.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import './reserve.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import usefetch from '../../hooks/usefetch'
+
+
 
 
 function Reserve({setopen, id}) {
 
     //set open is passed as a prop for the modal opening and closing while id is the id for the specific hotel that has been opened.
+
+
+    const {data, loading, error} = usefetch(`http://localhost:3001/api/hotels/room/${id}`)
+
+    console.log(data)
 
 
   return (
@@ -22,6 +30,20 @@ function Reserve({setopen, id}) {
                 <FontAwesomeIcon icon ={faCircleXmark} className= 'rclose' onClick ={()=>setopen(false)}/>
 
                 <span>Select your rooms: </span>
+
+                {data.map(item=>{
+
+                  <div className="ritem">
+
+                    <div className="rtitle">{item.title}</div>
+                    <div className="rdesc">{item.description}</div>
+
+
+
+                  </div>
+
+
+                })}
 
 
 
