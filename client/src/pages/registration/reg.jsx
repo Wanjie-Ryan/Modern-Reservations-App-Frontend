c69658f779bc9454f5a0e3c095303252d3b2b2de
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-22 00:10:01 +0300

    reg

--- client/src/pages/registration/reg.jsx
+++ client/src/pages/registration/reg.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './reg.css'
-
-
-
-
+import {RegContext} from '../../context/regcontext'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 
 
