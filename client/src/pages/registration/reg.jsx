4f007b63a46510ca14a1bdf49dcde154b1e70d53
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-21 23:52:06 +0300

    registration

--- client/src/pages/registration/reg.jsx
+++ client/src/pages/registration/reg.jsx
@@ -24,6 +24,8 @@ function Reg() {
         <div className="mainreg">
 
 
+            <label>Register to Mystic Travels</label>
+
             <div className="inner-reg">
 
                 <div className="inner">
@@ -32,13 +34,29 @@ function Reg() {
                 </div>
 
 
+                <div className="inner">
+                    <label>Email:</label>
+                    <input type="email" placeholder = 'email' id = 'email' className="reg" />
+                </div>
+
+                <div className="inner">
+                    <label>Password:</label>
+                    <input type="password" placeholder = 'password' id = 'password' className="reg" />
+                </div>
+
+
+                <div className="inner">
+                    <label>Admin:</label>
+                    <input type="checkbox" id = 'checkbox' className="reg" />
+                </div>
+
+
+                
+                <button className="btn">Register</button>
 
 
 
             </div>
-
-
-
 
 
 
